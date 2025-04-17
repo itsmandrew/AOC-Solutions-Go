7225f76ffbba99e848550993b38ebed0ca972931
Andrew Chang <theachang1688@gmail.com>
2025-04-17 13:19:03 -0700

    Finished part 1

--- 2020/day3/main.go
+++ 2020/day3/main.go
@@ -1,15 +1,64 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 
 	if len(os.Args[1:]) > 1 {
-		fmt.Printf("Got %d arguments, expected\n", len(os.Args[1:]))
+		fmt.Printf("Got %d arguments, expected 1\n", len(os.Args[1:]))
 		os.Exit(-1)
 	}
 
+	file, err := os.Open(os.Args[1])
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	defer file.Close()
+
+	matrix := createMatrix(file)
+
+	trees := countTreesInMatrix(matrix)
+	fmt.Printf("Trees in map: %d\n", trees)
+
+}
+
+func createMatrix(file *os.File) [][]string {
+	// Creates our data structure to iterate on
+
+	scanner := bufio.NewScanner(file)
+	matrix := [][]string{}
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), "")
+		matrix = append(matrix, line)
+	}
+
+	return matrix
+}
+
+func countTreesInMatrix(matrix [][]string) int {
+	// Given matrix, counts how many trees
+	trees := 0
+
+	ROWS, COLS := len(matrix), len(matrix[0])
+	rc, cc := 0, 0
+
+	for i := 0; i < ROWS-1; i++ {
+		rc, cc = rc+1, cc+3
+		if matrix[rc%ROWS][cc%COLS] == "#" {
+			trees += 1
+		}
+	}
+	return trees
+}
+
+func countTreesInMatrix_v2(matrix [][]string) int {
+	return 0
 }
--- 2020/day3/main_test.go
+++ 2020/day3/main_test.go
@@ -1,10 +1,47 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+)
 
 func TestBasics(t *testing.T) {
 
-	t.Run("default", func(t *testing.T) {
+	fname := "test_input/test1.txt"
+	t.Run("test 1 pass", func(t *testing.T) {
+
+		file, err := os.Open(fname)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		matrix := createMatrix(file)
+
+		got := countTreesInMatrix(matrix)
+		want := 7
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+
+	})
+
+	t.Run("test 2 pass", func(t *testing.T) {
+
+		file, err := os.Open(fname)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		matrix := createMatrix(file)
+
+		got := countTreesInMatrix_v2(matrix)
+		want := 336
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
 
 	})
 }
