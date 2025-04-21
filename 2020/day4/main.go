30c0369dd3e926280b301ca6d64c3dbaf44d7e60
itsmandrew <theachang1688@gmail.com>
2025-04-21 14:09:47 -0700

    finish day 4

--- 2020/day4/main.go
+++ 2020/day4/main.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
 func main() {
+
+	if len(os.Args[1:]) == 0 {
+		fmt.Println("Please give a file name, exiting...")
+		return
+	}
 
 	if len(os.Args[1:]) > 1 {
 		fmt.Println("Too many arguments, exiting...")
@@ -29,10 +36,18 @@ func main() {
 		fmt.Printf("%v\n", err)
 	}
 
-	required := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+	// required := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
 	hashMap := parsePassports(string(data))
 
-	fmt.Printf("%v\n", checkValid(hashMap, required))
+	res := 0
+
+	for _, entry := range hashMap {
+		if checkValidFields(entry) {
+			res++
+		}
+	}
+
+	fmt.Printf("valid passports: %d\n", res)
 }
 
 func parsePassports(input string) []map[string]string {
@@ -55,22 +70,89 @@ func parsePassports(input string) []map[string]string {
 	return res
 }
 
-func checkValid(hashMap []map[string]string, reqs []string) int {
-	validPassports := 0
+func checkValidKeys(hashMap map[string]string, reqs []string) bool {
 
-	for _, entry := range hashMap {
-		okFlag := true
-		fmt.Printf("%v\n", entry)
-		for _, key := range reqs {
-			if _, ok := entry[key]; !ok {
-				okFlag = false
-				break
-			}
+	var flag bool
+
+	for _, key := range reqs {
+		flag = true
+		if _, ok := hashMap[key]; !ok {
+			flag = false
+			break
 		}
-		if okFlag {
-			validPassports++
+	}
+	return flag
+}
+
+func checkValidFields(hashMap map[string]string) bool {
+
+	var validators = map[string]func(string) bool{
+
+		"byr": func(v string) bool {
+			if len(v) > 4 {
+				return false
+			}
+			y, err := strconv.Atoi(v)
+			return err == nil && 1920 <= y && y <= 2002
+		},
+
+		"iyr": func(v string) bool {
+			if len(v) > 4 {
+				return false
+			}
+			y, err := strconv.Atoi(v)
+			return err == nil && 2010 <= y && y <= 2020
+		},
+
+		"eyr": func(v string) bool {
+			if len(v) > 4 {
+				return false
+			}
+			y, err := strconv.Atoi(v)
+			return err == nil && 2020 <= y && y <= 2030
+		},
+
+		"hgt": func(v string) bool {
+			if strings.HasSuffix(v, "cm") {
+				x, err := strconv.Atoi(strings.TrimSuffix(v, "cm"))
+				return err == nil && 150 <= x && x <= 193
+			}
+
+			if strings.HasSuffix(v, "in") {
+				x, err := strconv.Atoi(strings.TrimSuffix(v, "in"))
+				return err == nil && 59 <= x && x <= 76
+			}
+			return false
+		},
+		"hcl": func(v string) bool {
+			m, _ := regexp.MatchString(`^#[0-9a-f]{6}$`, v)
+			return m
+		},
+		"ecl": func(v string) bool {
+			switch v {
+			case "amb", "blu", "brn", "gry", "grn", "hzl", "oth":
+				return true
+			}
+			return false
+		},
+		"pid": func(v string) bool {
+			m, _ := regexp.MatchString(`^[0-9]{9}$`, v)
+			return m
+		},
+	}
+
+	for key := range validators {
+		val, ok := hashMap[key]
+
+		if !ok {
+			return false
+		}
+
+		check := validators[key]
+		if !(check(val)) {
+			return false
 		}
 	}
 
-	return validPassports
+	return true
 }
