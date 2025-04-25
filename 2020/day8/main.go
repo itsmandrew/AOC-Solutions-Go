2b651508cabdc9ba0f756eba60b67404ae1513fa
itsmandrew <theachang1688@gmail.com>
2025-04-25 16:20:32 -0700

    Finished part one of day 8

--- 2020/day8/main.go
+++ 2020/day8/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/itsmandrew/aoc-go/utils"
 )
+
+type Tuple struct {
+	Command string
+	Value   int
+}
 
 func main() {
 
@@ -19,5 +26,63 @@ func main() {
 		fmt.Printf("%v\n", err)
 	}
 
-	fmt.Printf("%v\n", data)
+	arr := createMapping(data)
+
+	res := getAccumulator(arr)
+	fmt.Printf("%v\n", res)
+}
+
+func createMapping(lines []string) []Tuple {
+
+	var arr []Tuple
+
+	for _, line := range lines {
+		newLine := strings.Split(line, " ")
+
+		newNum, err := parseStringToInt(newLine[1])
+
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		}
+
+		tuple := Tuple{newLine[0], newNum}
+
+		arr = append(arr, tuple)
+	}
+
+	return arr
+}
+
+func parseStringToInt(val string) (int, error) {
+	return strconv.Atoi(val)
+}
+
+func getAccumulator(arr []Tuple) int {
+
+	visited := map[int]bool{}
+
+	accumulator := 0
+	i := 0
+
+	for {
+
+		if _, ok := visited[i]; ok {
+			break
+		}
+		visited[i] = true
+
+		switch arr[i].Command {
+		case "nop":
+			i++
+		case "acc":
+			accumulator += arr[i].Value
+			i++
+		case "jmp":
+			i += arr[i].Value
+		}
+
+	}
+
+	return accumulator
+
 }
