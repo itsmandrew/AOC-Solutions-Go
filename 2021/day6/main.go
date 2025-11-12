84d6b71c2a35331b3b44a22ad6870ed9812ca6a0
Andrew Chang <89715746+itsmandrew@users.noreply.github.com>
2025-11-12 13:34:08 -0800

    file read for day6

--- 2021/day6/main.go
+++ 2021/day6/main.go
@@ -1,4 +1,44 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ReadFileToArray(path string) []int {
+	var result []int
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+
+		line := scanner.Text()
+
+		arr := strings.Split(line, ",")
+
+		for _, val := range arr {
+
+			valInt, err := strconv.Atoi(val)
+			if err != nil {
+				log.Fatal(err)
+			}
+			result = append(result, valInt)
+		}
+	}
+	return result
+}
+
 func main() {
+	arr := ReadFileToArray("inputs/sample.txt")
+	fmt.Printf("%v\n", arr)
 }
