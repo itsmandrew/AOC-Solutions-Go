18d8003e470728985304a06cad753454d45b4bf5
Andrew Chang <89715746+itsmandrew@users.noreply.github.com>
2025-11-13 11:59:46 -0800

    Finished day 6

--- 2021/day6/main.go
+++ 2021/day6/main.go
@@ -38,7 +38,56 @@ func ReadFileToArray(path string) []int {
 	return result
 }
 
+// O(days * state) -> Can prob cut this down to O(days)
+func RunProgram(state []int, days int) int {
+	for i := 0; i < days; i++ {
+		for index := range state {
+			if state[index] == 0 {
+				state[index] = 6
+				state = append(state, 8)
+
+			} else {
+				state[index] -= 1
+			}
+		}
+	}
+
+	return len(state)
+}
+
+func RunProgramOptimized(state []int, days int) int {
+	result := 0
+	hashMap := make(map[int]int)
+	for _, val := range state {
+		hashMap[val]++
+	}
+	for i := 0; i < days; i++ {
+
+		newMap := make(map[int]int)
+
+		for timer, count := range hashMap {
+			if timer == 0 {
+				newMap[6] += count
+				newMap[8] += count
+			} else {
+				newMap[timer-1] += count
+			}
+		}
+		hashMap = newMap
+	}
+
+	for key := range hashMap {
+		result += hashMap[key]
+	}
+
+	return result
+}
+
 func main() {
-	arr := ReadFileToArray("inputs/sample.txt")
+	arr := ReadFileToArray("inputs/test.txt")
 	fmt.Printf("%v\n", arr)
+
+	result := RunProgramOptimized(arr, 256)
+
+	fmt.Printf("Fish: %v\n", result)
 }
